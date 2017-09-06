c815c3d7f59f16037dd94dd6d7b969c255c21eaf
Ben Rudolph <rudolphben@gmail.com>
2017-09-06 15:14:23 -0700

    field_trips#index

--- app/controllers/field_trips_controller.rb
+++ app/controllers/field_trips_controller.rb
@@ -6,4 +6,12 @@ class FieldTripsController < ApplicationController
       format.json { render json: { field_trip: @field_trip } }
     end
   end
+
+  def index
+    @field_trips = FieldTrip.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { field_trips: @field_trips } }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :field_trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index'
+  root 'field_trips#index'
 end
