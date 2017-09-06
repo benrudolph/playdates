c5c8748d0f97e1809ea4aaa79890cd3cd91524dd
Ben Rudolph <rudolphben@gmail.com>
2017-09-05 23:15:19 -0700

    WIP home.jsx

---
+++ app/javascript/packs/home.jsx
@@ -0,0 +1,35 @@
+import jQuery from 'jquery'
+
+import Tether from 'tether/dist/js/tether'
+import moment from 'moment/moment'
+import _ from 'underscore/underscore'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import { Provider } from 'react-redux'
+
+window.jQuery = jQuery
+window.moment = moment
+
+let store;
+const loggerMiddleware = createLogger()
+
+document.addEventListener('DOMContentLoaded', () => {
+  store = createStore(
+    null,
+    {},
+    applyMiddleware(
+      loggerMiddleware,
+    )
+  )
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <div>Hello</div>
+    </Provider>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
