cffb50cb4f0eb4bef3d0b086756bb818dbc8004a
Ben Rudolph <rudolphben@gmail.com>
2017-09-07 13:36:48 -0700

    create reservations via admin panel

---
+++ app/admin/reservations.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Reservation do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  permit_params :trip_date_id, :name, :email, :number_of_children
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
