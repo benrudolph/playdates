63f0112b7d0fec21ad58d720490c8b80d4373ce0
Ben Rudolph <rudolphben@gmail.com>
2017-09-06 17:57:59 -0700

    minus the s

--- db/migrate/20170907005428_create_trip_dates.rb
+++ db/migrate/20170907005428_create_trip_dates.rb
@@ -2,7 +2,7 @@ class CreateTripDates < ActiveRecord::Migration[5.1]
   def change
     create_table :trip_dates do |t|
       t.date :trip_date
-      t.belongs_to :field_trips
+      t.belongs_to :field_trip
 
       t.timestamps
     end
