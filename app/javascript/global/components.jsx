c60688d5787df849076c735310356cf0ca411b27
Ben Rudolph <rudolphben@gmail.com>
2017-09-07 17:57:46 -0700

    Better top bar

--- app/javascript/global/components.jsx
+++ app/javascript/global/components.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+import Logo from '../../assets/images/logo.png'
+
 
 export class MainNav extends React.Component {
     render() {
         return (
-            <nav className="navbar ft-navbar">
-              <div className="navbar-brand">
-                <a className="navbar-brand" href="/">FieldTrips</a>
-              </div>
+            <nav className="navbar ft-navbar d-flex justify-content-between">
+                <a className="navbar-brand" href="/">
+                    <img src={Logo} width="50" height="50" alt="" />
+                </a>
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                  </li>
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">PlayDates</a>
+                  </li>
+                </ul>
             </nav>
         )
     }
