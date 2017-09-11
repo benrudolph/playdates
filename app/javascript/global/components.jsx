fc2a3262bb4aebca1d67e4aedbb56017228b9a3e
Ben Rudolph <rudolphben@gmail.com>
2017-09-11 10:33:26 -0400

    Active class for header

--- app/javascript/global/components.jsx
+++ app/javascript/global/components.jsx
@@ -6,24 +6,45 @@ import { connect } from 'react-redux'
 
 import Logo from '../../assets/images/logo.png'
 
+const Tabs = [
+    {
+        controller: 'field_trips',
+        name: 'PlayDates',
+        url: '/',
+    },
+    {
+        controller: 'caregivers',
+        name: 'Our Caregivers',
+        url: '/caregivers',
+    },
+    {
+        controller: 'about',
+        name: 'About',
+        url: '/',
+    },
+]
 
 export class MainNav extends React.Component {
     render() {
+        const links = Tabs.map((tab, i) => {
+            let klass = 'nav-item'
+            if (this.props.controller === tab.controller) {
+                klass += ' active'
+            }
+            return (
+                <li key={i} className={klass}>
+                    <a className="nav-link" href={tab.url}>{tab.name}</a>
+                </li>
+            )
+        })
+
         return (
             <nav className="navbar ft-navbar d-flex justify-content-between">
                 <a className="navbar-brand" href="/">
                     <img src={Logo} width="50" height="50" alt="" />
                 </a>
                 <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <a className="nav-link" href="/">About</a>
-                  </li>
-                  <li className="nav-item active">
-                    <a className="nav-link" href="/">PlayDates</a>
-                  </li>
-                  <li className="nav-item active">
-                    <a className="nav-link" href="/caregivers">Our Caregivers</a>
-                  </li>
+                    {links}
                 </ul>
             </nav>
         )
@@ -34,7 +55,7 @@ export class MainNav extends React.Component {
 (function() {
     const mapStateToProps = state => {
         return {
-            fieldTrips: state.field_trips,
+            controller: state.controller,
         }
     }
 
