6ea47e4145a8f15942d76d7a3c13f9de047d4cfc
Ben Rudolph <rudolphben@gmail.com>
2017-09-11 09:18:34 -0400

    admin page for user (!)

---
+++ app/admin/user.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register User do
+
+  permit_params do
+    params = [:name, :email, :password, :about, :profile_image_url, :location, :profession]
+    params
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :about
+      f.input :profile_image_url
+      f.input :location
+      f.input :profession
+    end
+
+    f.actions
+  end
+end
